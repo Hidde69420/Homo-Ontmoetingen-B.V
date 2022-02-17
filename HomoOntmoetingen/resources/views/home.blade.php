9e6fee329fbd6ab8eea17aec0530110ccb0b123f
Hidde <hiddealberts@gmail.com>
2022-02-17 23:28:14 +0100

    started on the home page

---
+++ HomoOntmoetingen/resources/views/home.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homo Ontmoetingen B.V</title>
+</head>
+
+<body>
+    <div class="navbar">
+        <div class="col-3">
+            <a href="home">Home</a>
+        </div>
+        <div class="col-3">
+            <a href="onzehomos">Onze Homos</a>
+        </div>
+        <div class="col-3">
+            <a href="blog">Blog</a>
+        </div>
+        <div class="col-3">
+            <a href="contact">Contact</a>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <h1>HEY HOMO!!!!!!</h1>
+                    <p>misschien hoort u dit vaker misschien vindt u dit naar?
+                        hier bij Homo Ontmoetingen B.V doen wij niet aan. Wij zijn 100% LGBTQ+ vriendelijk, onze host
+                        Machiel Ten Bergen oftewel de M.T.B helpt u graag!!!! of dat nou een bdsm pakje is die je zoekt
+                        of misschien wel een lekkere blowjob?
+                        Homo Ontmoetingen B.V helpt bij onze LGBTQ+ stripclub kunt u lekker uw zak legen (of uw kut voor
+                        de lesbos onder ons) of uw nieuwe strap on halen bij onze gift shop.
+                    </p>
+                </div>
+                <div class="col-4">
+                    <img class="img-fluid" src={{ url('/storage/images/man-flag.jpg') }} alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <h1>HEY ik ben Machiel Ten Bergen (M.T.B)</h1>
+                    <p>
+                        text
+                    </p>
+                </div>
+                <div class="col-4">
+                    <img class="img-fluid" src={{ url('/storage/images/machiel.jpeg') }} alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="footer">
+        <p>BierPloeg Webdesing B.V</p>
+        <p>Homo Ontmoetingen B.V</p>
+    </div>
+    </div>
+</body>
+
+</html>
